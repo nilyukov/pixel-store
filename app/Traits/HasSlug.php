e410d040a0fd11ee63009981918835e4410e3059
nilyukov <nilyukov@gmail.com>
2024-04-23 16:41:50 +0300

    feat(Database): HasSlug trait unique

--- app/Traits/HasSlug.php
+++ app/Traits/HasSlug.php
@@ -3,18 +3,42 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 trait HasSlug
 {
     protected static function bootHasSlug(): void
     {
-        static::creating(function (Model $item) {
-            $item->slug = $item->slug ?? Str::slug($item->{self::slugFrom()});
-        });
+        static::creating(fn (Model $item) => $item->makeSlug());
     }
 
-    public static function slugFrom(): string
+    protected function makeSlug(): void {
+        if (!$this->slug) {
+            $this->slug = $this->slugUnique(
+                str($this->{$this->slugFrom()})
+                    ->slug()
+                    ->value()
+            );
+        }
+    }
+
+    protected function slugUnique(string $slug): string {
+        $originalSlug = $slug;
+        $i = 0;
+
+        while ($this->isSlugExists($slug)) {
+            $i++;
+
+            $slug = $originalSlug . '-' . $i;
+        }
+
+        return $slug;
+    }
+
+    protected function isSlugExists(string $slug): bool {
+        return $this->newQuery()->where('slug', $slug)->withoutGlobalScopes()->exists();
+    }
+
+    protected function slugFrom(): string
     {
         return 'title';
     }
