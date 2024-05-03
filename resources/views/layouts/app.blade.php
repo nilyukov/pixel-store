98f147be6cbdb5127fbf98b88a56df6a3faa2e23
nilyukov <nilyukov@gmail.com>
2024-05-03 15:23:35 +0300

    feat(Views): Add app/auth layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="favicon-16x16.png">
+    <title>Pixel Store</title>
+
+    @vite(['resources/css/app.css', 'resources/sass/main.sass', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+    @yield('content')
+</body>
+</html>
