99979459d462ccb11055a888051684b5b9b22d89
nilyukov <nilyukov@gmail.com>
2024-05-03 15:30:44 +0300

    feat(Providers): Add ViewServiceProvider for Vite image

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
-    App\Providers\TestingServiceProvider::class
+    App\Providers\TestingServiceProvider::class,
+    App\Providers\ViewServiceProvider::class,
 ];
