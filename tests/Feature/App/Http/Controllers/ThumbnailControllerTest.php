00539b9f0f2ce30783bce7b89ed78d0aca68151d
nilyukov <nilyukov@gmail.com>
2024-06-19 20:44:09 +0300

    feat(Testing): ThumbnailControllerTest

---
+++ tests/Feature/App/Http/Controllers/ThumbnailControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Database\Factories\ProductFactory;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class ThumbnailControllerTest extends TestCase
+{
+    public function test_generated_success()
+    {
+        $size = '500x500';
+        $method = 'resize';
+        $storage = Storage::disk('images');
+
+        config()->set('thumbnail.allowed_sizes', [$size]);
+
+        $product = ProductFactory::new()->create();
+
+        $response = $this->get($product->makeThumbnail($size, $method));
+
+        $response->assertOk();
+
+        $storage->assertExists(
+            "products/$method/$size/" . File::basename($product->thumbnail)
+        );
+    }
+}
