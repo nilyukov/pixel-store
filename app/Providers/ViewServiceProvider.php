3f573776b4a62e5e538ba4259c0aa2ccfe56e712
nilyukov <nilyukov@gmail.com>
2024-06-19 19:39:20 +0300

    feat(Views): Create Menu classes for views from ViewServiceProvider

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Menu\Menu;
+use App\Menu\MenuItem;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +24,13 @@ class ViewServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::macro('image', fn($asset) => $this->asset('resources/images/' . $asset));
+
+        View::composer('*', fn($view) => (
+            $view->with(
+                'menu',
+                Menu::make()
+                    ->add(MenuItem::make(route('home'), 'Главная'))
+            )
+        ));
     }
 }
--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -19,11 +19,7 @@
                         </svg>
                     </button>
                 </form>
-                <nav class="hidden 2xl:flex gap-8">
-                    <a href="{{ route('home') }}" class="text-white hover:text-pink font-bold">Главная</a>
-                    <a href="#" class="text-white hover:text-pink font-bold">Каталог товаров</a>
-                    <a href="#" class="text-white hover:text-pink font-bold">Корзина</a>
-                </nav>
+                @include('shared.menu')
             </div>
             <div class="header-actions flex items-center gap-3 md:gap-5">
                 @auth
