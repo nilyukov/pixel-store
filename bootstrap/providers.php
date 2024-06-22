050cbcc13b3a16c447a0dfb4532bdb5f84754edd
nilyukov <nilyukov@gmail.com>
2024-06-22 14:04:15 +0300

    feat(Filters): Filter for catalog

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\DomainServiceProvider::class,
+    \App\Providers\CatalogServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     App\Providers\TestingServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
