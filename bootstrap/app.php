80c0b151ec18c387336942ac251fd609990f0d94
nilyukov <nilyukov@gmail.com>
2024-07-15 12:24:34 +0300

    feat(Schedule): Cart daily mass prunable

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Schedule;
 use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -15,4 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
-    })->create();
+    })->create()
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('model:prune')->daily();
+    });
--- src/Domain/Cart/Models/Cart.php
+++ src/Domain/Cart/Models/Cart.php
@@ -2,18 +2,26 @@
 
 namespace Domain\Cart\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
     use HasFactory;
+    use MassPrunable;
 
     protected $fillable = [
         'storage_id',
         'user_id',
     ];
+
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->addDay());
+    }
 
     public function cartItems(): HasMany
     {
